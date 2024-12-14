7a3a58a62efd7e87fb70de00a55fb3d5491be22a
ThienDuc <ntduc4@gmail.com>
2024-12-14 22:43:40 +0700

    Global user, navbar change based on login

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import { Trophy } from "lucide-react";
 import { Link } from "react-router";
+import { useAtom } from "jotai";
+import { userAtom } from "@/context/user";
+
+interface NavbarOption {
+  to: string;
+  text: string;
+}
+const SignInOptions: NavbarOption[] = [
+  {
+    text: "Browse leaderboards",
+    to: "/leaderboard",
+  },
+  {
+    text: "Create new leaderboard",
+    to: "/leaderboard/new",
+  },
+  {
+    text: "Profile",
+    to: "/profile/me",
+  },
+];
+
+const SignOutOptions: NavbarOption[] = [
+  {
+    text: "Browse leaderboards",
+    to: "/leaderboard",
+  },
+  {
+    text: "Sign in",
+    to: "/signin",
+  },
+  {
+    text: "Sign up",
+    to: "/signup",
+  },
+];
 
 const NavBar: React.FC = () => {
+  const [user] = useAtom(userAtom);
   return (
     <nav className="bg-white shadow-sm">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
@@ -14,30 +51,18 @@ const NavBar: React.FC = () => {
         </div>
         <nav>
           <ul className="flex space-x-4">
-            <li>
-              <Link
-                to={"/leaderboard"}
-                className="text-gray-600 hover:text-indigo-600"
-              >
-                Browse leaderboard
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"/signin"}
-                className="text-gray-600 hover:text-indigo-600"
-              >
-                Sign in
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"/signup"}
-                className="text-gray-600 hover:text-indigo-600"
-              >
-                Sign up
-              </Link>
-            </li>
+            {(user ? SignInOptions : SignOutOptions).map((option, i) => {
+              return (
+                <li key={i}>
+                  <Link
+                    to={option.to}
+                    className="text-gray-600 hover:text-indigo-600"
+                  >
+                    {option.text}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </div>
--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,13 +1,30 @@
 import Button from "@/components/ui/button";
 import Input from "@/components/ui/textInput";
+import { userAtom } from "@/context/user";
+import { useAtom } from "jotai";
 import React from "react";
+import { useNavigate } from "react-router";
 
 const SignIn: React.FC = () => {
+  const navigate = useNavigate();
+  const [, setUser] = useAtom(userAtom);
   return (
     <div className="w-full mt-32 flex justify-center items-center">
       <div className="border border-indigo-200 rounded-xl bg-indigo-50">
         <h1 className="font-bold text-2xl text-center my-5">Sign in</h1>
-        <form className="flex flex-col py-3 px-5 my-1">
+        <form
+          className="flex flex-col py-3 px-5 my-1"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setUser({
+              displayName: "huyen",
+              email: "test@mail.com",
+              id: "adlsjkf",
+              username: "huyen",
+            });
+            navigate("/leaderboard");
+          }}
+        >
           <label htmlFor="username">Username or email address</label>
           <Input id="username" />
 
